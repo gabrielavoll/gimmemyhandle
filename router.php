ac1cad352db77ff3fde23ebcce87985d95c7e521
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-17 00:43:23 -0500

    now its a super small php server app

---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+	$request = $_SERVER['REQUEST_URI'];
+	$jsFiles = ['/main.js'];
+	$cssFile = '/styles.css';
+	$imageFiles = ['/images/twitter.png','/images/instagram.png','/images/twitter_unavailable.png', '/images/twitter_available.png', '/images/instagram_unavailable.png','/images/instagram_available.png'];
+
+	if($request == '/' || $request == ''){
+		require __DIR__ . '/index.html';
+	} else if ( $request == $cssFile ){
+		header("Content-type: text/css; charset: UTF-8");
+		require __DIR__ . $request;
+	} else if ( in_array( $request, $validFiles ) ){
+		require __DIR__ . $request;
+	} else if ( in_array($resquest, $imageFiles) ){
+		header(‘Content-Type: image/png’);
+		require __DIR__ . $request;
+	}
+?>
