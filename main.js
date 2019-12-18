abbf64f4198edfb7c935858846c3f421387b7801
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-18 14:50:09 -0500

    added error messaging for handle consume, and state on loading state on button, switch on api call url for herokuapp vs local testing, fixed javascript function being called when passed as variables

--- main.js
+++ main.js
@@ -1,58 +1,98 @@
 window.onload  = function (){
 	var input = document.getElementById("handleToCheck");
-
 	input.addEventListener("keyup", function(event) {
 	  if (event.keyCode === 13) {
 	    event.preventDefault();
 	    clickHandler();
-	  } else {
-	  	resetImageIndicators();
-	  }
+	  } else resetImageIndicators();
+	  return;
 	});
+}
+
+function buttonWaitingState(){
+	var button = document.getElementById('button');
+	button.innerHTML = "...loading";
+	button.disabled = true;
+	return;
+}
+
+function buttonReadyState(){
+	var button = document.getElementById('button');
+	button.innerHTML = "Search";
+	button.disabled = false;
+	return;
 }
 
 function clickHandler(){
 	resetImageIndicators();
-	document.getElementById('button').innerHTML = "...loading";
-	var handle = document.getElementById('handleToCheck').value;
-	console.log('check handl', handle)
-	getRequest( window.document.URL + 'check.php?handle=' + handle);
+	setError("");
+	var handle = document.getElementById('handleToCheck').value,
+		url = 'check';
+	if(window.document.URL.includes('herokuapp')) url = 'check.php';
+	if(handle == "") return setError("Please provide a handle to search")
+	var twitterValid = validTwitterHandle(handle);
+	var instagramValid = validInstagramHandle(handle);
+	if(!twitterValid && !instagramValid) return;
+	buttonWaitingState();
+	getRequest( window.document.URL + url + '?handle=' + handle, instagramValid, twitterValid);
+	return;
 }
 
 function resetImageIndicators(){
 	document.getElementById('twitter').src = "/images/twitter.png";
 	document.getElementById('instagram').src = "/images/instagram.png";
+	return;
 }
 
 function twitterResponseHandler(unavailable){
-		if(!unavailable){
-			document.getElementById('twitter').src = "/images/twitter_available.png";
-			return console.log("twitter avail");
-		}
-		document.getElementById('twitter').src = "/images/twitter_unavailable.png";
-		return console.log('twitter NOT avail');
+	if(!unavailable) document.getElementById('twitter').src = "/images/twitter_unavailable.png";
+	else document.getElementById('twitter').src = "/images/twitter_available.png";
+	return;
 }	
 
-function instagramResponseHandler(unavailable){
-		if(!unavailable){
-			document.getElementById('instagram').src = "/images/instagram_available.png";
-			return console.log("instagram avail");
-		}
-		document.getElementById('instagram').src = "/images/instagram_unavailable.png";
-		return console.log('instagram NOT avail');
+function setError(error){
+	document.getElementById('error').innerHTML = error;
 }
 
-function getRequest(url, callback){
+function validTwitterHandle(handle){
+	if(handle.length > 15){
+		setError("Twitter handle can only be up to 15 characters long.")
+		return false;
+	} else if( /^[A-z0-9_]{1,15}$/.test(handle) == false ){
+		setError("Twitter handle can only be alphanumeric characters(A-Z, 0-9) and underscores.")
+		return false;
+	}
+	return true;
+}
+
+function validInstagramHandle(handle){
+	if(handle.length > 30){
+		setError("Instagram handle can only be up to 30 characters long.")
+		return false;
+	} else if( /^[A-z0-9_.]{1,30}$/.test(handle) == false ){
+		setError("Twitter handle can only be alphanumeric characters(A-Z, 0-9), underscores and periods.")
+		return false;
+	}
+	return true;
+}
+
+function instagramResponseHandler(unavailable){
+	if(!unavailable) document.getElementById('instagram').src = "/images/instagram_unavailable.png";
+	else document.getElementById('instagram').src = "/images/instagram_available.png";
+	return;
+}
+
+function getRequest(url, showInstaResponse, showTwitterResponse){
 	var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    	console.log('maybe success', JSON.stringify(xmlHttp.responseText));
-    	var res = JSON.parse(xmlHttp.responseText);
-    	instagramResponseHandler(res.instagram);
-    	twitterResponseHandler(res.twitter);
-    	document.getElementById('button').innerHTML = "Search";
-    }
+	    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+	    	var res = JSON.parse(xmlHttp.responseText);
+	    	if(showInstaResponse) instagramResponseHandler(res.instagram);
+	    	if(showTwitterResponse) twitterResponseHandler(res.twitter);
+	    	buttonReadyState();
+	    }
+	}
     xmlHttp.open("GET", url, true);
     xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*')
-    xmlHttp.send(null);
+    xmlHttp.send(null);	
 }
